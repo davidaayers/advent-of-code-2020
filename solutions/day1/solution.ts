daa95a0bbfa9a4acb755839fbbf981f6bd3d22ce
David Ayers <david.ayers@invitationhomes.com>
2020-12-01 09:45:31 -0700

    Moar efficiency!

--- solutions/day1/solution.ts
+++ solutions/day1/solution.ts
@@ -1,3 +1,4 @@
+import { performance } from "perf_hooks";
 import { read } from "promise-path";
 import { fromHere, report as reportGen } from "../../util";
 
@@ -13,17 +14,16 @@ export async function run(day: string) {
 }
 
 async function solveForFirstStar(input) {
-  var lines:number[] = input.split('\n').map(line => {return +line});
+  var lines:number[] = input.split('\n').map(line => {return +line}).sort();
   var solution;
 
-  lines.forEach((v1,idx) => {
-    lines.slice(idx+1).forEach(v2 => {
-      if( v1 + v2 === 2020 ) {
-        solution = (v1 * v2).toString();
-      }
-    });
-  });
-
+  for( var v1 in lines) {
+    var v2 = 2020 - lines[v1];
+    if (lines.includes(v2)) {
+      solution = (lines[v1] * v2).toString();
+      break;
+    }    
+  }
   report("Solution 1:", solution);
 }
 
