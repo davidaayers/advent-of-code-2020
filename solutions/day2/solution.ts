fa854d1e2f1d21922062fa5cc5835659a4f9a48e
David Ayers <david.ayers@invitationhomes.com>
2020-12-02 10:26:45 -0700

    Now with moar types

--- solutions/day2/solution.ts
+++ solutions/day2/solution.ts
@@ -4,21 +4,28 @@ import { fromHere, report as reportGen } from "../../util";
 
 const report = reportGen(__filename);
 
+type Password = {
+  firstNum: number,
+  secondNum: number,
+  letter: string,
+  password: string,
+}
+
 export async function run(day: string) {
   const input = (
     await read(fromHere(`solutions/${day}` + "/input.txt"), "utf8")
   ).trim();
 
-  var passwords = input.split('\n').map(line=> {
+  var passwords:Array<Password> = input.split('\n').map(line => {
     // 15-16 m: mhmjmzrmmlmmmmmm
     const regex = /(\d+)-(\d+)\s(\w+):\s(\w+)/g;
 
     var result = regex.exec(line);
     if ( result === null) return false;
 
-    var o = {
-      firstNum: result[1],
-      secondNum: result[2],
+    var o : Password = {
+      firstNum: +result[1],
+      secondNum: +result[2],
       letter: result[3],
       password: result[4]
     }
@@ -34,20 +41,20 @@ export async function run(day: string) {
   report('Second star took ' + (performance.now()-secondStarStart)+ 'ms');
 }
 
-async function solveForFirstStar(passwords) {  
+async function solveForFirstStar(passwords:Array<Password>) {  
   var solution = passwords.filter( pw => {
     var numLetters = pw.password.split('').filter(letter => letter === pw.letter).length
     return numLetters >= pw.firstNum && numLetters <= pw.secondNum;
   }).length;
-  report("First star solution:", solution);
+  report("First star solution:", solution.toString());
 }
 
-async function solveForSecondStar(passwords) {
+async function solveForSecondStar(passwords:Array<Password>) {
   var solution = passwords.filter( pw => {
     var letterAtPos1 = pw.password.substring(pw.firstNum-1,pw.firstNum);
     var letterAtPos2 = pw.password.substring(pw.secondNum-1,pw.secondNum);
     return (letterAtPos1 === pw.letter || letterAtPos2 === pw.letter) &&
       !(letterAtPos1 === pw.letter && letterAtPos2 === pw.letter);
   }).length;
-  report("Second star solution:", solution);
+  report("Second star solution:", solution.toString());
 }
