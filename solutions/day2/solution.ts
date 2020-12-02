da7d827cf61eed9b3916a58ae9c40142dde9f3d9
David Ayers <david.ayers@invitationhomes.com>
2020-12-01 23:00:26 -0700

    Day 2 & general cleanup

--- solutions/day2/solution.ts
+++ solutions/day2/solution.ts
@@ -9,40 +9,49 @@ export async function run(day: string) {
     await read(fromHere(`solutions/${day}` + "/input.txt"), "utf8")
   ).trim();
 
-  var firstStarStart = performance.now();
-  await solveForFirstStar(input);
-  report('First star took ' + (performance.now()-firstStarStart)+ 'ms');
-  var secondStarStart = performance.now();
-  await solveForSecondStar(input);
-  report('Second star took ' + (performance.now()-secondStarStart)+ 'ms');
-}
-
-async function solveForFirstStar(input) {
   var passwords = input.split('\n').map(line=> {
     var parts = line.split(':');
     var password = parts[1].trim();
     var rulesParts = parts[0].split(' ');
     var requiredLetter = rulesParts[1];
     var countParts = rulesParts[0].split('-');
-    var min = +countParts[0];
-    var max = +countParts[1];
+    var firstNum = +countParts[0];
+    var secondNum = +countParts[1];
     return {
-      min: min,
-      max: max,
+      firstNum: firstNum,
+      secondNum: secondNum,
       letter: requiredLetter,
       password: password
     }
   })
-  
+
+  var firstStarStart = performance.now();
+  await solveForFirstStar(passwords);
+  report('First star took ' + (performance.now()-firstStarStart)+ 'ms');
+  var secondStarStart = performance.now();
+  await solveForSecondStar(passwords);
+  report('Second star took ' + (performance.now()-secondStarStart)+ 'ms');
+}
+
+async function solveForFirstStar(passwords) {  
   var solution = passwords.filter( pw => {
     var numLetters = pw.password.split('').filter(letter => letter === pw.letter).length
-    return numLetters >= pw.min && numLetters <= pw.max;
-  }).length 
-
+    return numLetters >= pw.firstNum && numLetters <= pw.secondNum;
+  }).length;
   report("First star solution:", solution);
 }
 
-async function solveForSecondStar(input) {
-  const solution = "UNSOLVED";
+async function solveForSecondStar(passwords) {
+  var solution = passwords.filter( pw => {
+    var letterAtPos1 = pw.password.substring(pw.firstNum-1,pw.firstNum);
+    var letterAtPos2 = pw.password.substring(pw.secondNum-1,pw.secondNum);
+
+    console.log('Work on:' + JSON.stringify(pw));
+    console.log('pos1 ' + pw.firstNum + ' letter ' + letterAtPos1)
+    console.log('pos2 ' + pw.secondNum + ' letter ' + letterAtPos2)
+
+    return (letterAtPos1 === pw.letter || letterAtPos2 === pw.letter) &&
+      !(letterAtPos1 === pw.letter && letterAtPos2 === pw.letter);
+  }).length;
   report("Second star solution:", solution);
 }
